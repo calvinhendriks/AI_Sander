96d858f3f6386cf8245773cbb5539a2fd60cf7b1
Sander Bloemen <s.p.r.bloemen@student.utwente.nl>
2019-01-18 22:23:14 +0100

    first commit

--- AI Project/src/agent.py
+++ AI Project/src/agent.py
@@ -52,25 +52,25 @@ class Agent:
         return Move.STRAIGHT
     
     def heuristic(a, b):
-    (x1, y1) = a
-    (x2, y2) = b
-    return abs(x1 - x2) + abs(y1 - y2)
+        (x1, y1) = a
+        (x2, y2) = b
+        return abs(x1 - x2) + abs(y1 - y2)
 
     def a_star_search(graph, start, goal):
-    frontier = PriorityQueue()
-    frontier.put(start, 0)
-    came_from = {}
-    cost_so_far = {}
-    came_from[start] = None
-    cost_so_far[start] = 0
+        frontier = PriorityQueue()
+        frontier.put(start, 0)
+        came_from = {}
+        cost_so_far = {}
+        came_from[start] = None
+        cost_so_far[start] = 0
     
-    while not frontier.empty():
+        while not frontier.empty():
         current = frontier.get()
         
         if current == goal:
             break
         
-        for next in graph.neighbors(current):
+            for next in graph.neighbors(current):
             new_cost = cost_so_far[current] + graph.cost(current, next)
             if next not in cost_so_far or new_cost < cost_so_far[next]:
                 cost_so_far[next] = new_cost
@@ -78,7 +78,7 @@ class Agent:
                 frontier.put(next, priority)
                 came_from[next] = current
     
-    return came_from, cost_so_far
+                return came_from, cost_so_far
 
     def should_redraw_board(self):
         """
